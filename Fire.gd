685b977e0c1b0d565b695c3d2f8821ec0b47247c
Hakim Rouatbi <hakim.rouatbi@pernod-ricard.com>
2020-04-19 18:20:07 +0200

    Add light, Border walls & camera limit

--- Fire.gd
+++ Fire.gd
@@ -3,6 +3,7 @@ extends Node2D
 func toggle_fire():
 	$FireParticules.emitting = !$FireParticules.emitting
 	$SmokeParticles.emitting = !$SmokeParticles.emitting
+	$Light2D.visible = !$Light2D.visible
 
 func _input(event : InputEvent):
 	if event.is_action_pressed("ui_accept"):
